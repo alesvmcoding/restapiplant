11b2f969620ba285aa8ab8b0c69b3f6b52e9ea01
alesvmcoding <alesvmelo@gmail.com>
2021-04-26 14:33:28 -0300

    api plantmanager

---
+++ index.js
@@ -0,0 +1,16 @@
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 8877;
+
+server.use(middlewares)
+server.use(router)
+server.listen(PORT, () => {
+    console.log('JSON Server is running')
+})
+
+
+
